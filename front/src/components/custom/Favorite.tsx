693e5dad313252d7c31546e3a10d86a0b7096fda
IANTSOGod <141714262+IANTSOGod@users.noreply.github.com>
2024-10-21 07:53:01 +0300

    Delete front directory

--- front/src/components/custom/Favorite.tsx
+++
@@ -1,9 +0,0 @@
-import { Label } from "../ui/label";
-
-export default function Favorite() {
-  return (
-    <div className="w-5/6 mx-auto mt-10">
-      <Label className="text-xl font-bold text-black">Top Picks for You</Label>
-    </div>
-  );
-}
